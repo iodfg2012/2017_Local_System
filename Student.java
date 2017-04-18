4923979717af02d506f6d6055c677e9f1ad045f6
iodfg2012 <iodfg2012@daum.net>
2017-04-18 15:39:47 +0900

    에시
    예시추가

---
+++ Student.java
@@ -0,0 +1,20 @@
+package text1;
+
+import java.util.*;
+
+public class Student {
+	
+	String name;
+	int a;
+
+	 public static void main(String[] args) {
+	    	
+	    	Student student = new Student();
+	    	Scanner sc = new Scanner(System.in);
+			 
+			 System.out.println("학과를 입력하세요");
+			 sc.next();
+			 System.out.println("학번을 입력하세요");
+			 sc.nextInt();
+	 }
+}
